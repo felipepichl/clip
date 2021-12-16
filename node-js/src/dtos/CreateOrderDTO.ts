07a8d5077340ca23192a5ea1e0424845986c022f
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-16 08:32:01 -0300

    Node-JS_API: Order DTO

---
+++ node-js/src/dtos/CreateOrderDTO.ts
@@ -0,0 +1,5 @@
+interface ICreateOrderDTO {
+  budget: number;
+}
+
+export { ICreateOrderDTO };
--- node-js/src/repositories/OrdersRepository.ts
+++ node-js/src/repositories/OrdersRepository.ts
@@ -1,22 +1,15 @@
 import { v4 as uuidV4 } from 'uuid';
 
+import { ICreateOrderDTO } from '../dtos/CreateOrderDTO';
 import { Order } from '../model/Order';
-
-type OrderDTO = {
-  id?: string;
-
-  budget: number;
-
-  created_at: Date;
-};
 
 class OrderRepository {
   private orders: Order[] = [];
 
-  public async create(orderData: OrderDTO): Promise<Order> {
+  public async create({ budget }: ICreateOrderDTO): Promise<Order> {
     const order = new Order();
 
-    Object.assign(order, { id: uuidV4() }, orderData);
+    Object.assign(order, { id: uuidV4() }, { budget, created_at: new Date() });
 
     this.orders.push(order);
 
