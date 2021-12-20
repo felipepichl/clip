f3304ba6c8cbb9b40d5d06446acd6879c9914465
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-20 06:29:17 -0300

    Node-JS_API: Order create service in route

--- node-js/src/routes/orders.routes.ts
+++ node-js/src/routes/orders.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { OrderRepository } from '../repositories/OrdersRepository';
+import { CreateOrderServices } from '../services/CreateOrderServices';
 
 const ordersRouter = Router();
 const repository = new OrderRepository();
@@ -8,13 +9,9 @@ const repository = new OrderRepository();
 ordersRouter.post('/', (request, response) => {
   const { budget } = request.body();
 
-  const budgetAlreadyExists = repository.findByBudget(budget);
+  const service = new CreateOrderServices(repository);
 
-  if (budgetAlreadyExists) {
-    return response.status(400).json({ error: 'Budget already exists' });
-  }
-
-  repository.create({
+  service.execute({
     budget,
   });
 
