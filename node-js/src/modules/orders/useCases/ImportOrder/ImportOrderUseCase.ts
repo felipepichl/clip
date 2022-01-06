f3629f467056b7e5fe54f35b13e389a8ee04045f
Felipe Pichl <felipe.pichl@hotmail.com>
2022-01-05 23:57:44 -0300

    Node-JS_API: Doing Parse

--- node-js/src/modules/orders/useCases/ImportOrder/ImportOrderUseCase.ts
+++ node-js/src/modules/orders/useCases/ImportOrder/ImportOrderUseCase.ts
@@ -1,10 +1,30 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
 interface IRequest {
-  file: any;
+  file: Express.Multer.File;
+}
+
+interface IImportOrder {
+  product: string;
+  valor: string;
 }
 
 class ImportOrderUseCase {
+  // loadProduct({ file }: IRequest): Promise<IImportOrder[]>{
+
+  // };
+
   execute({ file }: IRequest): void {
-    console.log(file);
+    const steam = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    steam.pipe(parseFile);
+
+    parseFile.on('data', (line) => {
+      console.log(line);
+    });
   }
 }
 
