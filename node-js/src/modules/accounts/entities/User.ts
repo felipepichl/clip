4d56a28aa0715804d9c7d46df722e1621d61867c
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-18 23:17:03 -0300

    Node-JS_API: Users entity

---
+++ node-js/src/modules/accounts/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  private readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
