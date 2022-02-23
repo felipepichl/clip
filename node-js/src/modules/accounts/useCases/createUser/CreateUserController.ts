f92b0ed441e22905e6c55a70d70954f2ffacffde
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-22 22:48:48 -0300

    Node-JS_API: CreateUserController

--- node-js/src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ node-js/src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
 
 class CreateUserController {
-  async handle(request: Request, response: Response): Promise<void> {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, phone } = request.body;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    await createUserUseCase.execute({
+      name,
+      phone,
+    });
+
+    return response.status(200).send();
+  }
 }
 
 export { CreateUserController };
