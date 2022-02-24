abc9b6fc059072d093dfdf45487369fbe02d29ba
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-23 21:50:45 -0300

    Node-JS_API: User routes

--- node-js/src/routes/index.ts
+++ node-js/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { ordersRouter } from './orders.routes';
+import { userRoutes } from './users.routes';
 
 const router = Router();
 
 router.use('/orders', ordersRouter);
+router.use('/users', userRoutes);
 
 export { router };
