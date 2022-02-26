5ba35b87ad53e37c8b0588bedc250c4de3ef40bd
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-25 23:54:07 -0300

    Node-JS_API: Doing error middleware

--- node-js/src/start/server.ts
+++ node-js/src/start/server.ts
@@ -1,15 +1,33 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import '../database';
 import '../shared/container';
 
 import { router } from '../routes';
+import { AppError } from '../shared/errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (err: Error, resquest: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error = ${err}`,
+    });
+  }
+);
 
 app.listen(3333, () => {
   console.log('Server running in port 3333');
