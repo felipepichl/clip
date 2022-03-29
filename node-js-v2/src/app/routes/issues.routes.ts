f44bedcd11f82087c0f4283d262de9192aa4167f
Felipe Pichl <felipe.pichl@hotmail.com>
2022-03-28 21:36:53 -0300

    Node-JS_APIv2: Issues; routes

--- node-js-v2/src/app/routes/issues.routes.ts
+++ node-js-v2/src/app/routes/issues.routes.ts
@@ -2,4 +2,18 @@ import { Router } from 'express';
 
 const issuesRoutes = Router();
 
+const issues = [];
+
+issuesRoutes.post('', (request, response) => {
+  const { description, latitude, longitude } = request.body;
+
+  issues.push({
+    description,
+    latitude,
+    longitude,
+  });
+
+  return response.status(201);
+});
+
 export { issuesRoutes };
--- node-js-v2/src/routes/index.ts
+++ node-js-v2/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
-import { createUserRouter } from '../app/routes/user.routes';
+import { issuesRoutes } from '../app/routes/issues.routes';
 import { sessionRouter } from '../app/routes/session.routes';
+import { createUserRouter } from '../app/routes/user.routes';
 
 const router = Router();
 
 router.use('/users', createUserRouter);
-
 router.use('/session', sessionRouter);
+
+issuesRoutes.use('issues', issuesRoutes);
 
 export default router;
