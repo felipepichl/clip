e9c318b5fa0e561fb4e416177ce4c2a547f34c45
Felipe Pichl <felipe.pichl@hotmail.com>
2022-04-17 21:00:58 -0300

    Node-JS_APIv2: Update migration

--- node-js-v2/src/shared/infra/typeorm/migrations/1625602948696-AddPasswordFiledToUsers.ts
+++ node-js-v2/src/shared/infra/typeorm/migrations/1625602948696-AddPasswordFiledToUsers.ts
@@ -1,7 +1,8 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export default class AddPasswordFiledToUsers1625602948696
-  implements MigrationInterface {
+  implements MigrationInterface
+{
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
       'users',
