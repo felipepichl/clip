bce356e81bea220f54c97ae6efb9ce4a1532c9a6
Felipe Pichl <felipe.pichl@hotmail.com>
2022-06-14 19:10:04 -0300

    Node-JS_APIv2: TypeORM mapping

--- node-js-v2/src/modules/accounts/entities/User.ts
+++ node-js-v2/src/modules/accounts/entities/User.ts
@@ -1,15 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+@Entity('users')
 class User {
+  @PrimaryColumn()
   private readonly id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @Column()
   password: string;
 
+  @CreateDateColumn()
   created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   constructor() {
     if (!this.id) {
--- node-js-v2/src/modules/issues/entities/Issue.ts
+++ node-js-v2/src/modules/issues/entities/Issue.ts
@@ -1,15 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+@Entity('issues')
 class Issue {
+  @PrimaryColumn()
   private readonly id: string;
 
+  @Column()
   description: string;
 
+  @Column()
   latitude: number;
 
+  @Column()
   longitude: number;
 
+  @CreateDateColumn()
   created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   constructor() {
     if (!this.id) {
