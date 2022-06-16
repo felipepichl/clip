b44dbb0538cab5ccd4eadd84551ac01b8ce3c706
Felipe Pichl <felipe.pichl@hotmail.com>
2022-06-16 11:15:27 -0300

    Node-JS_APIv2: Dependency Injection, container

--- node-js-v2/src/shared/container/index.ts
+++ node-js-v2/src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { UsersRepository } from '@modules/accounts/repositories/implementations/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IIssuesRepository } from '@modules/issues/repositories/IIssuesRepository';
+import { IssuesRepository } from '@modules/issues/repositories/implementations/IssuesRepository';
 import { container } from 'tsyringe';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IIssuesRepository>(
+  'IssuesRepository',
+  IssuesRepository,
 );
