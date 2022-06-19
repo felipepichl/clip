68b42be6081329fca6f48a7aee5517db738a9b62
Felipe Pichl <felipe.pichl@hotmail.com>
2022-06-18 22:15:26 -0300

    Node-JS_APIv2: Routes update

--- node-js-v2/src/modules/issues/routes/issues.routes.ts
+++ node-js-v2/src/modules/issues/routes/issues.routes.ts
@@ -3,13 +3,13 @@ import { Router } from 'express';
 import { CreateIssueController } from '../useCases/createIssue/CreateIssueController';
 import { ListIssuesController } from '../useCases/listIssues/ListIssuesController';
 
-const issuesRoutes = Router();
+const issuesRouter = Router();
 
 const createIssueController = new CreateIssueController();
 const listIssuesController = new ListIssuesController();
 
-issuesRoutes.post('/', createIssueController.handle);
+issuesRouter.post('/', createIssueController.handle);
 
-issuesRoutes.get('/', listIssuesController.handle);
+issuesRouter.get('/', listIssuesController.handle);
 
-export { issuesRoutes };
+export { issuesRouter };
--- node-js-v2/src/shared/infra/http/routes/index.ts
+++ node-js-v2/src/shared/infra/http/routes/index.ts
@@ -1,15 +1,11 @@
 import { usersRouter } from '@modules/accounts/routes/users.routes';
-import { issuesRoutes } from '@modules/issues/routes/issues.routes';
+import { issuesRouter } from '@modules/issues/routes/issues.routes';
 import { Router } from 'express';
-
-// import { sessionRouter } from '../app/routes/session.routes';
-// import { createUserRouter } from '../app/routes/user.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
-// router.use('/session', sessionRouter);
 
-router.use('/issues', issuesRoutes);
+router.use('/issues', issuesRouter);
 
 export { router };
