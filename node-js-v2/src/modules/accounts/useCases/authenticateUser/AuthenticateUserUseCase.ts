ffe0871a64596bdca61ffc8b42db7a585c42fe11
Felipe Pichl <felipe.pichl@hotmail.com>
2022-06-20 23:25:20 -0300

    Node-JS_APIv2: Authenticate, useCase

--- node-js-v2/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ node-js-v2/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,10 +1,34 @@
+import { compare } from 'bcryptjs';
+import { inject } from 'tsyringe';
+
+import { AppError } from '@shared/error/AppError';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
 interface IRequest {
   email: string;
   password: string;
 }
 
 class AuthenticateUserUseCase {
-  async execute({ email, password }: IRequest) {}
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorret email/pawword combination');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError('Incorret email/pawword combination');
+    }
+  }
 }
 
 export { AuthenticateUserUseCase };
