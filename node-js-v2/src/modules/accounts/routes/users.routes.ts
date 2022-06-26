1c8f031dd79a2b23bbdd675d7447973dab14750c
Felipe Pichl <felipe.pichl@hotmail.com>
2022-06-26 20:23:37 -0300

    Node-JS_APIv2: Doing uploadAvatar route

--- node-js-v2/src/modules/accounts/routes/users.routes.ts
+++ node-js-v2/src/modules/accounts/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../useCases/createUser/CreateUserController';
+import { UploadUserAvatarController } from '../useCases/uploadUserAvatar/UploadUserAvatarController';
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
+const uploadUserAvatarController = new UploadUserAvatarController();
 
 usersRouter.post('', createUserController.handle);
+
+usersRouter.patch('/avatar', uploadUserAvatarController.handle);
 
 export { usersRouter };
