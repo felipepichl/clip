155ce46de81e2359f319084103666790dab73f87
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-01 16:13:35 -0300

    Node-JS_APIv2: InMemory

---
+++ node-js-v2/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,29 @@
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  private users: User[] = [];
+
+  async create(data: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, { id: '' }, data);
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find(user => user.email === email);
+
+    return user;
+  }
+
+  async findById(user_id: string): Promise<User> {
+    const user = this.users.find(user => user.id === user_id);
+
+    return user;
+  }
+}
+
+export { UsersRepositoryInMemory };
