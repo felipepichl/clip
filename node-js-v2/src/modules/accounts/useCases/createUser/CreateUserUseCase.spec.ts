484eb3db25bd2e264c4923f5ed2eb2402d6a083e
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-04 13:55:48 -0300

    Node-JS_APIv2: Done all users tests

--- node-js-v2/src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
+++ node-js-v2/src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,22 +1,27 @@
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+
+import { AppError } from '@shared/error/AppError';
+
 import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
 import { CreateUserUseCase } from './CreateUserUseCase';
 
 let usersRepositoryInMemory: UsersRepositoryInMemory;
 let createUserUseCase: CreateUserUseCase;
+let user: ICreateUserDTO;
 
 describe('Create a user', () => {
   beforeEach(() => {
     usersRepositoryInMemory = new UsersRepositoryInMemory();
     createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
-  });
 
-  it('should be able to create a new user', async () => {
-    const user = {
+    user = {
       name: 'John Due',
       email: 'jonhdue@example.com',
       password: 'hsh123',
     };
+  });
 
+  it('should be able to create a new user', async () => {
     await createUserUseCase.execute(user);
 
     const { email } = user;
@@ -24,5 +29,13 @@ describe('Create a user', () => {
     const userCreated = await usersRepositoryInMemory.findByEmail(email);
 
     expect(userCreated).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email another', async () => {
+    await createUserUseCase.execute(user);
+
+    await expect(createUserUseCase.execute(user)).rejects.toBeInstanceOf(
+      AppError,
+    );
   });
 });
