552069178c42ce87e4dd840d0d81e55674a20a58
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-05 18:00:19 -0300

    Node-JS_APIv2: Authenticate test

---
+++ node-js-v2/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+
+describe('Authenticate User', () => {
+  beforeAll(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory,
+    );
+  });
+
+  it('should be able to authenticate an user', async () => {
+    const user: ICreateUserDTO = {
+      name: 'John Due',
+      email: 'jonh.due@example.com',
+      password: 'hash123',
+    };
+
+    await createUserUseCase.execute(user);
+
+    const { email, password } = user;
+
+    const response = await authenticateUserUseCase.execute({
+      email,
+      password,
+    });
+
+    expect(response).toHaveProperty('token');
+  });
+});
