e8777ff450e74982ae1951fa25b7f0bf2ab869f8
Felipe Pichl <felipe.pichl@hotmail.com>
2022-07-08 13:46:00 -0300

    Node-JS_APIv2: HashProvider, container

--- node-js-v2/src/shared/container/index.ts
+++ node-js-v2/src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import '@modules/accounts/providers';
+
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { IssuesRepository } from '@modules/issues/infra/typeorm/repositories/IssuesRepository';
