70565a84cb8b839281ae4818be58bee498eba0af
Felipe Pichl <felipe.pichl@hotmail.com>
2022-08-01 20:53:41 -0300

    Node-JS_APIv2: CreateUserController Test

---
+++ node-js-v2/src/modules/accounts/infra/http/controllers/CreateUserController.spec.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+
+import { app } from '@shared/infra/http/start/app';
+import { AppDataSource } from '@shared/infra/typeorm';
+
+describe('Create User Controller', () => {
+  beforeEach(async () => {
+    await AppDataSource.initialize();
+    await AppDataSource.runMigrations();
+  });
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'John Due',
+      email: 'john.due@example.com',
+      password: 'hash_123',
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
