1e5f9a96f659da0acbb2b4e216a77e5cb1749d00
Felipe Pichl <felipe.pichl@hotmail.com>
2023-06-19 16:56:10 -0300

    NJS-Contacts: Doing useCase Part I

--- contacts/node-js/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ contacts/node-js/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,4 @@
-import { IHashProvider } from '@modules/accounts/providers/HashProvider/models/IHashProvider';
+import { User } from '@modules/accounts/domain/User';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { inject, injectable } from 'tsyringe';
 
@@ -6,8 +6,8 @@ import { AppError } from '@shared/error/AppError';
 
 interface IRequest {
   name: string;
-  email: string;
-  password: string;
+  cpf: string;
+  whatsapp: string;
 }
 
 @injectable()
@@ -15,24 +15,22 @@ class CreateUserUseCase {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
-    @inject('HashProvider')
-    private hashProvider: IHashProvider,
   ) {}
 
-  async execute({ name, email, password }: IRequest): Promise<void> {
-    const user = await this.usersRepository.findByEmail(email);
+  async execute({ name, cpf, whatsapp }: IRequest): Promise<void> {
+    const userAllReadyExists = await this.usersRepository.findByCpf(cpf);
 
-    if (user) {
+    if (userAllReadyExists) {
       throw new AppError('Users already exists', 400);
     }
 
-    const hashedPassword = await this.hashProvider.generateHash(password);
-
-    await this.usersRepository.create({
+    const user = User.createUser({
       name,
-      email,
-      password: hashedPassword,
+      cpf,
+      whatsapp,
     });
+
+    await this.usersRepository.create(user);
   }
 }
 
