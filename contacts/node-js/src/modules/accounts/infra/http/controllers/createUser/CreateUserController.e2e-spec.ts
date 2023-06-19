0c5531f10a3f14221c19c223bf4e8e50e60bbba6
Felipe Pichl <felipe.pichl@hotmail.com>
2023-06-19 16:46:39 -0300

    NJS-Contacts: Update all tests for createUsers

--- contacts/node-js/src/modules/accounts/infra/http/controllers/createUser/CreateUserController.e2e-spec.ts
+++ contacts/node-js/src/modules/accounts/infra/http/controllers/createUser/CreateUserController.e2e-spec.ts
@@ -5,9 +5,9 @@ import { app } from '@shared/infra/http/start/app';
 describe('[E2E] = Create User', () => {
   it('should be able to create a new user', async () => {
     const response = await request(app).post('/users').send({
-      name: 'John Due',
-      email: 'john.due@example.com',
-      password: 'hash_123',
+      name: 'Jonh Due',
+      cpf: '111.222.333-44',
+      whatsapp: '5555999999999',
     });
 
     expect(response.status).toBe(201);
--- contacts/node-js/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
+++ contacts/node-js/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,27 +1,22 @@
-import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
-import { User } from '@modules/accounts/infra/prisma/models/User';
+import { User } from '@modules/accounts/domain/User';
 
 import { IUsersRepository } from '../IUsersRepository';
 
 class UsersRepositoryInMemory implements IUsersRepository {
   private users: User[] = [];
 
-  async create(data: ICreateUserDTO): Promise<void> {
-    const user = new User();
-
-    Object.assign(user, data);
-
+  async create(user: User): Promise<void> {
     this.users.push(user);
   }
 
-  async findByEmail(email: string): Promise<User> {
-    const user = this.users.find(user => user.email === email);
+  async findByCpf(cpf: string): Promise<User> {
+    const user = this.users.find(user => user.cpf === cpf);
 
     return user;
   }
 
   async findById(user_id: string): Promise<User> {
-    const user = this.users.find(user => user.id === user_id);
+    const user = this.users.find(user => user.id.toString() === user_id);
 
     return user;
   }
