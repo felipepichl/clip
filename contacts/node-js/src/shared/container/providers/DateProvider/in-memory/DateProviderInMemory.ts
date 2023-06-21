2a61f1af6a67a653710ddca6a8d0e732cae52e4d
Felipe Pichl <felipe.pichl@hotmail.com>
2023-06-21 15:16:06 -0300

    NJS-Contacts: Clean project

--- contacts/node-js/src/shared/container/providers/DateProvider/in-memory/DateProviderInMemory.ts
+++
@@ -1,9 +0,0 @@
-import { IDateProvider } from '../models/IDateProvider';
-
-class DateProviderInMemory implements IDateProvider {
-  addDays(days: number): Date {
-    return new Date(days);
-  }
-}
-
-export { DateProviderInMemory };
--- contacts/node-js/src/shared/container/providers/StorageProvider/models/IStorageProvider.ts
+++
@@ -1,6 +0,0 @@
-interface IStorageProvider {
-  saveFile(file: string, folder: string): Promise<string>;
-  deleteFile(file: string, folder: string): Promise<void>;
-}
-
-export { IStorageProvider };
