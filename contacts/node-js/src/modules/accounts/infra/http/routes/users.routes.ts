20d6f5eb5c2ddf19ff8769588115ca3e4eec1fe1
Felipe Pichl <felipe.pichl@hotmail.com>
2023-06-23 16:30:47 -0300

    NJS-Contacts: Clean project and mongodb config

--- contacts/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
+++ contacts/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
@@ -1,16 +1,10 @@
-import { uploadConfig } from '@config/upload';
 import { CreateUserController } from '@modules/accounts/infra/http/controllers/createUser/CreateUserController';
 import { Router } from 'express';
-import multer from 'multer';
 
 const usersRouter = Router();
-
-const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 
 usersRouter.post('', createUserController.handle);
-
-usersRouter.patch('/avatar', uploadAvatar.single('avatar'));
 
 export { usersRouter };
