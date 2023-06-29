478e5a4e0d3c23d7b17fdf2d9bcd4722d843d1c4
Felipe Pichl <felipe.pichl@hotmail.com>
2023-06-29 18:21:39 -0300

    NJS-Contacts: NJS-Contacts: Implement listUserByCPF feature

--- contacts/node-js/src/modules/accounts/infra/http/controllers/listUserByCPF/ListUserByCPFController.e2e-spec.ts
+++ contacts/node-js/src/modules/accounts/infra/http/controllers/listUserByCPF/ListUserByCPFController.e2e-spec.ts
@@ -3,8 +3,8 @@ import { app } from '@shared/infra/http/start/app'
 
 describe('[E2E] - List user by CPF', () => {
   it('should be able to retrieve user information by CPF', async () => {
-    await request(app).post('/user').send({
-      name: 'Jonh Due',
+    await request(app).post('/users').send({
+      name: 'John Due',
       cpf: '111.222.333-44',
       whatsapp: '5555999999999',
     })
@@ -12,6 +12,6 @@ describe('[E2E] - List user by CPF', () => {
     const response = await request(app).get('/users/111.222.333-44')
 
     expect(response.status).toBe(200)
-    expect(response.body.name).toBe('John Due')
+    expect(response.body.user.props.name).toBe('John Due')
   })
 })
--- contacts/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
+++ contacts/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
@@ -1,10 +1,14 @@
-import { CreateUserController } from '@modules/accounts/infra/http/controllers/createUser/CreateUserController'
 import { Router } from 'express'
+
+import { CreateUserController } from '@modules/accounts/infra/http/controllers/createUser/CreateUserController'
+import { ListUserByCPFController } from '../controllers/listUserByCPF/ListUserByCPFController'
 
 const usersRouter = Router()
 
 const createUserController = new CreateUserController()
+const listUserByCPFController = new ListUserByCPFController()
 
 usersRouter.post('', createUserController.handle)
+usersRouter.get('/:cpf', listUserByCPFController.handle)
 
 export { usersRouter }
