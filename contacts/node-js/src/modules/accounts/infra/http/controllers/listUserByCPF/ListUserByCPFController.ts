ebc6993856e752599a27bf7879d26e3a6319e2d4
Felipe Pichl <felipe.pichl@hotmail.com>
2023-07-04 17:36:06 -0300

    NJS-Contacts: Doing CPF replace and swegger

--- contacts/node-js/src/modules/accounts/infra/http/controllers/listUserByCPF/ListUserByCPFController.ts
+++ contacts/node-js/src/modules/accounts/infra/http/controllers/listUserByCPF/ListUserByCPFController.ts
@@ -6,9 +6,15 @@ class ListUserByCPFController {
   async handle(request: Request, response: Response) {
     const { cpf } = request.params
 
+    const cpfFormatted = cpf.replace('/[.-]/g', '')
+
+    console.log('====================================')
+    console.log(cpfFormatted)
+    console.log('====================================')
+
     const listUserByCPF = container.resolve(ListUserByCPF)
 
-    const user = await listUserByCPF.execute({ cpf })
+    const user = await listUserByCPF.execute({ cpf: cpfFormatted })
 
     return response.json(user)
   }
